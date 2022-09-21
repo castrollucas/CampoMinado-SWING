e2a58d4b433d5efc6790b3e63cb19fe26b4b6bd3
Lucas Castro <castrollucas@gmail.com>
2022-09-21 20:22:33 -0300

    Introduzido metodo Observer

---
+++ src/br/com/dec/cm/visao/BotaoCampo.java
@@ -0,0 +1,14 @@
+package br.com.dec.cm.visao;
+
+import javax.swing.JButton;
+
+import br.com.dec.cm.modelo.Campo;
+
+@SuppressWarnings("serial")
+public class BotaoCampo extends JButton {
+	
+	public BotaoCampo(Campo campo) {
+		
+	}
+
+}
